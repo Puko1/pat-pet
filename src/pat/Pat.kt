c40982bdc4543ab8fef6a910e06aba9e6662ef8d
Puko1 <porksodeli11@gmail.com>
2024-04-01 22:14:29 +0900

    blackhole

--- src/pat/Pat.kt
+++ src/pat/Pat.kt
@@ -14,16 +14,18 @@ class Pat : JFrame() {
 
     private var positionY = 0.0
     private var velocityY = 0.0
-    private val gravity = 0.07
+    private var gravity = 1.0f
     private val frameWidth = 150
     private val frameHeight = 150
     private var isOnGround = false
+    private var Blackhole = false
+    private var Blackholing = false
 
     init {
         title = "p"
         defaultCloseOperation = EXIT_ON_CLOSE
         isUndecorated = true
-        shape = RoundRectangle2D.Float(75f, 75f, frameWidth.toFloat(), frameHeight.toFloat(), 50f, 50f)
+        shape = RoundRectangle2D.Float(75f, 75f, frameWidth.toFloat(), frameHeight.toFloat(), 75f, 75f)
         size = Dimension(300, 300)
         setLocation(500, 0)
         val timer = Timer(10) { moveFrame() }
@@ -54,13 +56,18 @@ class Pat : JFrame() {
             val mouseLocation = MouseInfo.getPointerInfo().location
             val currentX = mouseLocation.x
             val currentY = mouseLocation.y
+            if (velocityY>0) {
+                gravity = 3.0f
+            } else {
+                gravity = 1.0f
+            }
             applyGravity(currentX, currentY)
         }
 
         if (isTouchingGround()) {
             handleGroundCollision()
 
-            val condition = kotlin.random.Random.nextInt(4)
+            val condition = kotlin.random.Random.nextInt(5)
             println(condition)
             when (condition) {
                 0 -> for (i in 50..100) {
@@ -81,7 +88,14 @@ class Pat : JFrame() {
 
                 3 -> {
                     isOnGround = false
-                    velocityY = -11.0
+                    velocityY = -43.0
+                }
+
+                4 -> {
+                    isOnGround = false
+                    Blackhole = true
+                    Blackholing = true
+                    velocityY = -0.1
                 }
             }
         } else {
@@ -90,12 +104,18 @@ class Pat : JFrame() {
     }
 
     private fun applyGravity(x: Int, y: Int) {
-        velocityY += gravity
-        setLocation(location.x, (location.y + velocityY).toInt())
-        if (location.y+225>=y && location.y+75<=y && location.x<=x && location.x+225>=x && velocityY > 0) {
-            val robot = Robot()
-            robot.mouseMove(x, location.y + 225)
+        if (Blackhole) {
+            Blackhole = false
+            isBlackhole()
+        } else if (!Blackholing){
+            velocityY += gravity
+            setLocation(location.x + (detectX(x)), (location.y + velocityY).toInt())
+            if (location.y + 225 >= y && location.y + 75 <= y && location.x + 75 <= x && location.x + 225 >= x && velocityY > 0) {
+                val robot = Robot()
+                robot.mouseMove(x, location.y + 225)
+            }
         }
+
     }
 
     private fun isTouchingGround(): Boolean {
@@ -108,6 +128,38 @@ class Pat : JFrame() {
         isOnGround = true
     }
 
-    private fun isJumping(y: Int) {
+    private fun detectX(mouseX: Int): Int {
+        if (location.x +75<=mouseX && location.x+225>=mouseX) {
+            return 0
+        } else {
+            if (mouseX > location.x +135 ) {
+                return 12
+            } else {
+                return -12
+            }
+        }
+    }
+
+    private fun isBlackhole() {
+        for (i in 1..900) {
+            setLocation(location.x, location.y - 1)
+            Thread.sleep(1)
+        }
+
+        val blackholeX = location.x + 75
+        val blackholeY = location.y + 75
+        background.icon = iconImage
+        for (i in 1..500) {
+            val currentMouseLocation = MouseInfo.getPointerInfo().location
+            val dx = (blackholeX - currentMouseLocation.x) / 75
+            val dy = (blackholeY - currentMouseLocation.y) / 75
+            val newX = currentMouseLocation.x + dx
+            val newY = currentMouseLocation.y + dy
+            val robot = Robot()
+            robot.mouseMove(newX.toInt(), newY.toInt())
+            Thread.sleep(10)
+        }
+        Blackhole = false
+        Blackholing = false
     }
 }
